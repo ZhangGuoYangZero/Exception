39b505b99744d521700ba44f642606f723583712
张国杨Zero <101501786+ZhangGuoYangZero@users.noreply.github.com>
2022-05-07 23:09:51 +0800

    普通修改

--- exceptionTest/src/test/java/test2.java
+++ exceptionTest/src/test/java/test2.java
@@ -5,16 +5,18 @@ import java.io.PrintWriter;
 
 public class test2 {
 
-    public static void main(String[] args) {
-         try {
+    public static void main(String[] args) throws Throwable {
              //这个显示的是全局的的项目编码。
              //我设置的项目编码是GBK， 控制台编码是UTF
              System.out.println(System.getProperty("file.encoding"));
-             System.out.println("抛");
-             throw new TestException("抓住了！！！");
-         }catch (TestException testException){
-             System.out.println(testException.getMessage());
-         }
+
+            int a = 5;
+            if( a < 4){}
+            else {
+                throw   new Exception("测试编码100112"){
+
+                };
+            }
     }
 
 }
